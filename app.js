6b06575ade5a3bd0e1c4cd236bc44b78ea8e5928
JOHN RAYOME <JOHN.RAYOME87@GMAIL.COM>
2022-04-28 12:05:54 -0700

    updatezs

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict"
+
+
+
+
+
+
+
+let userName = prompt("What is your name?");
+console.log(userName);
+
+if username = ("Kassie"||"Robert"||"Lizzie"){
+    console.log("This student deserves extra credit. Is that even possible?")
+} else {
+    console.log(userName + " welcome to my site.")
+}
+
+    
+
+let feeling = prompt ("Welcome " + userName + "How are you today");
+console.log(feeling);
+
+let response = alert("I hope you have a good experience with my webpage.")
+
+document.write("<p id='person-name'>" + username + "</p>");
