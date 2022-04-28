72bc38e2b7a72388e8943f52521f0d46544880a2
JOHN RAYOME <JOHN.RAYOME87@GMAIL.COM>
2022-04-28 14:21:11 -0700

    Unbreaking JS

---
+++ JS1.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let userName = prompt("What is your name?");
+console.log(userName);
+
+if (userName = "Kassie") {
+
+    console.log("This student deserves extra credit. Is that even possible?");
+
+} else if (userName = "Robert"){ 
+
+    console.log("This student deserves extra credit. Is that even possible?");
+
+} else if (userName = "Lizzie") {
+
+    console.log("This student deserves extra credit. Is that even possible?");
+
+} else {
+    console.log(userName + " welcome to my site.");
+}    
+    
+
+    
+
+let feeling = prompt (`Welcome ${userName}How are you today`);
+console.log(feeling);
+
+let response = alert("I hope you have a good experience with my webpage.");
+
+document.write("<p id='person-name'>" + userName + "</p>");
