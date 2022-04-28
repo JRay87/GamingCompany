19af42196cd5435e46804295c0760dd57486c286
JOHN RAYOME <JOHN.RAYOME87@GMAIL.COM>
2022-04-28 16:39:04 -0700

    CSS formatting was atrocious and JavaScript was broken

--- JS1.js
+++ JS1.js
@@ -1,29 +1,29 @@
 "use strict"
 
 let userName = prompt("What is your name?");
-console.log(userName);
+    console.log(userName);
 
-if (userName = "Kassie") {
-
-    console.log("This student deserves extra credit. Is that even possible?");
-
-} else if (userName = "Robert"){ 
+if (userName==="Kassie"){
 
     console.log("This student deserves extra credit. Is that even possible?");
 
-} else if (userName = "Lizzie") {
+} else if (userName==="Robert") { 
+
+    console.log("This student deserves extra credit. Is that even possible?");
+
+} else if (userName==="Lizzie") {
 
     console.log("This student deserves extra credit. Is that even possible?");
 
 } else {
-    console.log(userName + " welcome to my site.");
+    console.log(userName + " Johnwelcome to my site.");
 }    
     
 
     
 
-let feeling = prompt (`Welcome ${userName}How are you today`);
-console.log(feeling);
+let feeling = prompt ("Welcome, How are you today?"); 
+    console.log(feeling);
 
 let response = alert("I hope you have a good experience with my webpage.");
 
